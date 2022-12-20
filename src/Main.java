6840587b6a052210f1c10bf4f6ee9c47392508e1
Purcari <sabina.motrescu@gmail.com>
2022-12-20 19:39:37 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int number1 = 421;
+        int number2 = 225;
+        int number3 = 264;
+        int number4 = 333;
+        int number5 = 14;
+        int number6 = 1;
+        int number7 = 321;
+        int number8 = 452;
+        int sum;
+
+        sum = number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8;
+        System.out.print("The sum of the number is: ");
+        System.out.println(sum);
+
+
+
+
+
+    }
+}
